1bf5444375b25ced6809a7e53cf24ec5f5e35dee
panyq357 <panyq357@qq.com>
2021-12-27 20:02:06 +0800

    bug fix

--- R/get_CRDC_gene_url.R
+++ R/get_CRDC_gene_url.R
@@ -5,10 +5,13 @@
 #' @return corresponding gene info page URL in CRDC.
 #' @export
 #'
-get_CRDC_gene_info_page_url <- function(gene_id) {
+get_CRDC_gene_url <- function(gene_id) {
+    cat(sprintf("Processing: %s\n", gene_id))
     url <- stringr::str_c("https://www.ricedata.cn/gene/accessions_switch.aspx?para=", gene_id)
-    page <- rvest::read_html(url)
+    page <- rvest::read_html(url, encoding = "UTF-8")
     element <- rvest::html_element(page, "a")
     info_page_url <- rvest::html_attr(element, "href")
+    if (!is.na(info_page_url) & str_length(info_page_url) >= 4 & str_sub(info_page_url,1,4) == "list")
+        info_page_url <- str_c("https://www.ricedata.cn/gene/", info_page_url)
     return(info_page_url)
 }
